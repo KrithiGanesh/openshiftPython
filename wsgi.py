e68caafd2619a43791482124aa9435a35bdbba08
KrithiGanesh <50772393+KrithiGanesh@users.noreply.github.com>
2020-01-21 16:16:14 +0530

    speechrecogcode

--- wsgi.py
+++ wsgi.py
@@ -19,6 +19,8 @@ from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core import get_authenticator_from_environment
 
 import os
+import speech_recognition as sr
+import json
 
 
 
@@ -426,64 +428,18 @@ def search():
 
     if 'q' in request.args:
 
-       print ("in speech to text",)
-
+      print ("in speech to text",)
+      r = sr.Recognizer()
+      speech = sr.Microphone()
+      speech_to_text = SpeechToTextV1( iam_apikey = "AMyhXj5hTvqHyqNe4vt6e6uZiGCWFtR-5TqnGy3tpetJ",
+               url = "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/e854020d-4a48-40df-82d1-6981762b205e")
         
-       speech_to_text = SpeechToTextV1()
-
-
-
-       response = sttService.recognize(
-
-
-
-            audio=request.get_data(cache=False),
-
-
-
-            content_type='audio/wav',
-
-
-
-            timestamps=True,
-
-
-
-            word_confidence=True,
-
-
-
-            smart_formatting=True).get_result()
-
-
-
-          # Ask user to repeat if STT can't transcribe the speech
-
-
-
-       if len(response['results']) < 1:
-
-
-
-            return Response(mimetype='plain/text',response="Sorry, didn't get that. please try again!")
-
-
-
-            text_output = response['results'][0]['alternatives'][0]['transcript']
-
-
-
-            text_output = text_output.strip()
-
-
-
-            print ("response of speech is :",text_output)
-
-
-
-            return Response(response=text_output, mimetype='plain/text')
-
-          
+     with speech as source:
+        print("say something!!…")
+        audio_file = r.adjust_for_ambient_noise(source)
+        audio_file = r.listen(source)
+        speech_recognition_results = speech_to_text.recognize(audio=audio_file.get_wav_data(), content_type='audio/wav').get_result()
+        print(json.dumps(speech_recognition_results, indent=2))        
 
     if 'q' in request.args:
 
